60f401d925251d9b355877ad3c8a02522cd99541
dawko66 <73241237+dawko66@users.noreply.github.com>
2021-12-06 18:46:01 +0100

    Dodaj pliki projektów.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VODUser.Models.Identity;
+
+namespace VODUser.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registermodel)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    UserName = registermodel.Email,
+                    Email = registermodel.Email
+                };
+                IdentityResult result = await _userManager.CreateAsync(user, registermodel.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var err in result.Errors) { ModelState.AddModelError(string.Empty, err.Description); }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new  User
+                {
+                    Email = loginViewModel.Email,
+                    UserName = loginViewModel.Email
+                };
+
+
+                Microsoft.AspNetCore.Identity.SignInResult identityResult = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                
+                if (identityResult.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Niepoprawny login lub hasło.");
+                }
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult CheckLogin()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/Database.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VODUser.Models.Identity;
+
+namespace VODUser.Models
+{
+    public class Database : IdentityDbContext<User>
+    {
+        public Database(DbContextOptions<Database> options) : base(options)
+        {
+
+        }
+    }
+}
